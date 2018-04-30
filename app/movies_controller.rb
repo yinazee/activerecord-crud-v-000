b17bd99d612ee307b66832242877e767b034f19e
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:47:53 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -91,7 +91,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  Movie.destroy(title: "That One Where the Guy Kicks Another Guy Once")
   __
 end
 
