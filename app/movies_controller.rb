ba78efe71c30a29d5ab4d7ec899312eaddaab8d0
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:18:49 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,11 +33,11 @@ def can_be_created_in_a_block(args = {title: "I Feel Pretty", release_date: 2018
 end
 
 def can_get_the_first_item_in_the_database
-  movies.first.title
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movies.last.
 end
 
 def can_get_size_of_the_database
