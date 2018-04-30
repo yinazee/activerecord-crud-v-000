0180ca97b29d8cfe1ee5962d79825d81af11c02d
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:41:44 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -81,7 +81,9 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.all.each do |movie|
+    movie.update(title: "A Movie")
+  end
 end
 
 def can_destroy_a_single_item
