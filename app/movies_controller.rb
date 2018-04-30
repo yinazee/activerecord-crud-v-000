e6fce8ddebd9bb01778afcc6137afec929a7397c
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:21:25 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = {title: "I Feel Pretty", release_date: 2018})
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
