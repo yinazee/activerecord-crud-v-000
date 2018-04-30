18f3f8d6483fd14f8c9d6d18d4ec693911ff4518
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:19:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,11 +37,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  movies.last.
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
