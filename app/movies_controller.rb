7c56f89b5b7f3194b8db22f5bb8ea38eb2aa2a1b
Yina Zhu <yinagit@gmail.com>
2018-04-30 04:18:13 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,9 +24,11 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "I Feel Pretty", release_date: 2018})
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
@@ -54,7 +56,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -94,11 +96,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
